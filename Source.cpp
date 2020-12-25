be420040a03335476335d444be34ac7b78d8c1e0
VivianLiu108 <61656910+VivianLiu108@users.noreply.github.com>
2020-12-25 19:41:44 +0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -7,6 +7,6 @@ int main()
 	float x1, y1, x2, y2, x22, y22, x3, y3;
 	while (cin >> x1 >> y1 >> x2 >> y2 >> x22 >> y22 >> x3 >> y3)
 	{
-		cout << fixed << setprecision(3) << x1 + x3 - x2 << ' ' << y1 + y3 - y2 << endl;
+		cout << fixed << setprecision(3) << x1 + x3 - x2 << ' ' << y1 + y3 - y2 << endl; //setprecision(x):精準x位(含整數) + fixed ->精準至小數點下第x位
 	}
 }
